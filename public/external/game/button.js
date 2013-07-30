f48e8681ce5b7887d7e0b186d8b93aba3324a3d1
jamongkad <wrm932@gmail.com>
2013-07-30 23:38:10 +0800

    wqeq

--- public/external/game/button.js
+++ public/external/game/button.js
@@ -8,20 +8,22 @@ function Button(options) {
     this.activate = function() { 
         var cd = this.options.cooldown;
         var width = $(this.options.elem).width();
-        var current_width = width + "px";
+        var current_width = '50%';//width + "px";
         var target_width = '0%';
         var text = this.options.text;
         var actiontext = this.options.actiontext;
 
         var that = this;
+
+        console.log(current_width);
          
         if(!this.options.elem.hasClass("disabled")) { 
             this.options.elem
                         .stop(true, true)
                         .addClass("disabled")
                         .text(actiontext)
-                        .animate({width: target_width, padding: '4px 0px'}, cd * 1000, 'linear', function() { 
-                            $(this).text("done!").animate({padding: '4px 12px', width: current_width}, 1000, 'linear', function() {
+                        .animate({ width: target_width, padding: '4px 0px' }, cd * 1000, 'linear', function() { 
+                            $(this).text("done!").animate({ padding: '4px 12px', width: current_width }, 1000, 'linear', function() {
                                 $(this).text(text).attr("class", "btn btn-primary collect").removeAttr("style").removeClass("disabled");
                             });
 
