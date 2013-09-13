37ebc889f6695d80b70167353c8e4412b12ad9f8
jamongkad <wrm932@gmail.com>
2013-09-13 18:05:33 +0800

    addding bootbox

--- public/external/game/directives/dropzone.js
+++ public/external/game/directives/dropzone.js
@@ -3,7 +3,29 @@ angular.module('Dropzone', [])
     return {
         restrict: "EAC"     
       , link: function(scope, element, attrs) {
-          
+            $(element).droppable({
+                drop: function(ev, ui) {
+                    var anomaly = ui.draggable;
+                    var name = anomaly.children("div.panel-heading").attr('anomaly-name');
+                    var me = this;
+
+                    $.ajax({
+                        type: 'GET'    
+                      , dataType: 'json' 
+                      , url: '/visitor/' + name + '/speak'
+                      , success: function(data) {   
+                            /*
+                            scope.$apply(function() { 
+                                scope.person = data;
+                            });
+                            */
+                            console.log(data);
+                            anomaly.fadeOut();
+                            $(me).html("<p>" + data.request + "</p>");
+                        }
+                    });
+                }
+            })
         }
     }
 });
--- public/external/game/directives/nav.js
+++ public/external/game/directives/nav.js
@@ -1,10 +1,39 @@
 angular.module('Nav', [])
-.directive('mainnav', function() {  
+.directive('showClassifier', function() {  
     return {
         restrict: "A"     
       , link: function(scope, element, attrs) {
             $(element).bind("click", function(e) {  
-                console.log("Nav");
+                var html = "Category" + 
+                '<select name="category" class="form-control">' + 
+                '<option>Safe</option>' + 
+                '<option>Euclid</option>' + 
+                '<option>Keter</option>' + 
+                '<option>Anomalous</option>' + 
+                '</select><br/>' +
+                "Category" + 
+                '<select name="category" class="form-control">' + 
+                '<option>Safe</option>' + 
+                '<option>Euclid</option>' + 
+                '<option>Keter</option>' + 
+                '<option>Anomalous</option>' + 
+                '</select><br/>' + 
+                'Containment Procedure<textarea id="textArea" class="form-control" rows="10"></textarea>'
+
+                bootbox.dialog({
+                    message: html 
+                  , title: "SCP Classify Tool"
+                  , buttons: {
+                      success: { 
+                        label: "Accept" 
+                      , className: "btn-success"
+                      }
+                      , danger: {
+                         label: "Reject" 
+                       , className: "btn-danger"
+                      }
+                    }
+                });
             });
         }
     }
